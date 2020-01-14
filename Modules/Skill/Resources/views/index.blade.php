9af6a7362cf4266f6993334954c60a7868e0aec1
laravel.dev <basket.baza@gmail.com>
2020-01-15 01:07:32 +0300

    Add  PermissionServiceProvider override $user->hasPermissionTo() me van used $user->can(). Add Module Slill

---
+++ Modules/Skill/Resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('skill::layouts.master')
+
+@section('content')
+
+    @basket('asasfasfadfg')
+
+    <br>
+    @role('project-manager')
+    Project Manager Panel
+    @endrole
+    <br>
+    @role('web-developer')
+    Web Developer Panel
+    @endrole
+
+
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('skill.name') !!}
+    </p>
+@endsection
---
+++ app/Providers/PermissionServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use App\Permission;
+use App\Traits\HasRolesAndPermissions;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    use HasRolesAndPermissions;
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        try {
+            Permission::get()->map(function ($permission) {
+                Gate::define($permission->slug, function ($user) use ($permission) {
+                    return $user->hasPermissionTo($permission);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            return false;
+        }
+
+    }
+}
