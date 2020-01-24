8f39fdcdf48431bb674956d257910a749291d9d4
basket.baza@gmail.com <basket.baza@gmail.com>
2020-01-24 11:32:14 +0300

    Commit on work

---
+++ Modules/TreeLife/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('treelife::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('treelife.name') !!}
+    </p>
+@endsection
