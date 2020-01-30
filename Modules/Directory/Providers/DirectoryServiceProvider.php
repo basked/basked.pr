4096dad8a38c0f26ccdac3cb99053b1805f7060e
basket.baza@gmail.com <basket.baza@gmail.com>
2020-01-30 09:53:45 +0300

    Commit on work Diagram data

---
+++ Modules/Directory/Providers/DirectoryServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Directory\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class DirectoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Directory', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Directory', 'Config/config.php') => config_path('directory.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Directory', 'Config/config.php'), 'directory'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/directory');
+
+        $sourcePath = module_path('Directory', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/directory';
+        }, \Config::get('view.paths')), [$sourcePath]), 'directory');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/directory');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'directory');
+        } else {
+            $this->loadTranslationsFrom(module_path('Directory', 'Resources/lang'), 'directory');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Directory', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
