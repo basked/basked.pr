b2c834b4aa182d6e21e50b8285698938198bd1f1
basket.baza@gmail.com <basket.baza@gmail.com>
2020-01-31 10:47:50 +0300

    Commit on work Repository version 2 see https://laravel.demiart.ru/repository-design-pattern/

---
+++ Modules/Directory/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Directory\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Directory\Repositories\ContinentRepository;
+use Modules\Directory\Repositories\ContinentRepositoryApi;
+use Modules\Directory\Repositories\Interfaces\ContinentRepositoryInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *  Easy replace realisation from ContinentRepository::class on ContinentRepositoryApi::class
+     * @return void
+     */
+    public function register()
+    {
+       $this->app->bind(ContinentRepositoryInterface::class,ContinentRepositoryApi::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
