6291f335b2530abac0ad4534d3c168a8cbb07ca3
basket.baza@gmail.com <basket.baza@gmail.com>
2020-06-29 16:56:16 +0300

    Изменен метод обновления во всех Api-контроллерах

---
+++ database/backup/basked_db_sk_resources.sql
@@ -0,0 +1,8 @@
+INSERT INTO basked_db.sk_resources (id, text) VALUES (1, 'Management');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (2, 'Project Manager');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (3, 'Analyst');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (4, 'Developer');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (5, 'Testers');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (6, 'Trainers');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (7, 'Technical Communicators');
+INSERT INTO basked_db.sk_resources (id, text) VALUES (8, 'Deployment Team');
